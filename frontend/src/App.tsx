d459115aa1b94b384e1c75e0fad77b190d0259f8
DenysKomar <66992115+DenysKomar@users.noreply.github.com>
2023-02-08 00:46:03 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,7 +30,7 @@ import axios from "axios";
 import NotFound from "./pages/NotFound";
 
 function App() {
-  axios.defaults.baseURL = "window.location.href";
+ 
   const navigate = useNavigate();
   const { cart, user } = useSelector((state: RootState) => state);
   const signOutHandler = () => {
